91d4db5e540123e12d8714a6bee405ea8f45ba2a
Michelle Bang <bangm@oregonstate.edu>
2023-11-15 16:37:47 -0800

    updated so you can use the difficulty

--- microservice.js
+++ microservice.js
@@ -8,16 +8,41 @@ var port = 2000
 
 app.use(cors())
 
-function generateRandomNumber() {
+function generateEasyNumber() {
     num = Math.floor(Math.random() * 11)
+    return num.toString()
+}
+
+function generateMedNumber() {
+    num = Math.floor(Math.random() * (99 - 10 + 1) + 10)
+    return num.toString()
+}
+
+function generateHardNumber() {
+    num = Math.floor(Math.random() * (999 - 100 + 1) + 100)
     return num.toString()
 }
 
 
 app.get('/', (req, res) => {
-    var text = generateRandomNumber()
+    var text = "Random number generator!"
     res.send(text)
-  })
+})
+
+app.get('/easy', (req, res) => {
+    var text = generateEasyNumber()
+    res.send(text)
+})
+
+app.get('/medium', (req, res) => {
+    var text = generateMedNumber()
+    res.send(text)
+})
+
+app.get('/hard', (req, res) => {
+    var text = generateHardNumber()
+    res.send(text)
+})
 
 app.listen(port, function () {
     console.log("== Server is listening on port:", port)
--- request.js
+++ request.js
@@ -1,13 +1,35 @@
 /* Using Fetch API: 
 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
 
-async function getRandomNumber() {
+async function getEasyNumber() {
     console.log("Request Sent")
-    const response = await fetch("http://localhost:2000")
+    const response = await fetch("http://localhost:2000/easy")
     const number = await response.text()
     console.log(number)
     document.getElementById("problem").textContent = number
 }
 
-var button = document.getElementById("num-button")
-button.addEventListener("click", getRandomNumber)
+var easyButton = document.getElementById("easy-button")
+easyButton.addEventListener("click", getEasyNumber)
+
+async function getMedNumber() {
+    console.log("Request Sent")
+    const response = await fetch("http://localhost:2000/medium")
+    const number = await response.text()
+    console.log(number)
+    document.getElementById("problem").textContent = number
+}
+
+var medButton = document.getElementById("medium-button")
+medButton.addEventListener("click", getMedNumber)
+
+async function getHardNumber() {
+    console.log("Request Sent")
+    const response = await fetch("http://localhost:2000/hard")
+    const number = await response.text()
+    console.log(number)
+    document.getElementById("problem").textContent = number
+}
+
+var hardButton = document.getElementById("hard-button")
+hardButton.addEventListener("click", getHardNumber)
